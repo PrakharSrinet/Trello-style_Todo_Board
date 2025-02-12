fb183c7e7455ab482dc911e34a85d001cef6cad7
Prakhar Srinet <prakhar.srinet@kongsbergdigital.com>
2025-02-12 23:21:53 +0530

    Save current changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
-import { LaneComponent } from './components/lane/lane.component';
-import { TodoComponent } from './components/todo/todo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FormsModule } from '@angular/forms';
@@ -12,9 +10,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent,
-    LaneComponent,
-    TodoComponent
+    BoardComponent
   ],
   imports: [
     BrowserModule, 
